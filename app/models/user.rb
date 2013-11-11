dfa3daddb6ca38490f86c51411fd6a8445257775
alberthu <alberthu4@comcast.net>
2013-11-10 19:01:40 -0800

    Add user following

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,12 @@ class User < ActiveRecord::Base
   #attr_protected  :email, :name, :password, :password_confirmation
   #attr_accessible :email, :name
   has_many :microposts, dependent: :destroy
+  has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :followed_users, through: :relationships, source: :followed
+  has_many :reverse_relationships, foreign_key: "followed_id",
+                                   class_name:  "Relationship",
+                                   dependent:   :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
   has_secure_password
 
   before_save { self.email.downcase! }
@@ -17,7 +23,19 @@ class User < ActiveRecord::Base
   after_validation { self.errors.messages.delete(:password_digest) }
   def feed
     # This is preliminary. See "Following users" for the full implementation.
-    Micropost.where("user_id = ?", id)
+   Micropost.from_users_followed_by(self)
+  end
+
+  def following?(other_user)
+    relationships.find_by_followed_id(other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(followed_id: other_user.id)
+  end
+
+  def unfollow!(other_user)
+    relationships.find_by_followed_id(other_user.id).destroy
   end
 
   private
